0961bc739935bc898f8322cf41b861567c89c889
Gurucharan <gurucharan.healthguru@gmail.com>
2023-12-23 20:04:56 +0530

    after add header footer and nav

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+    def home 
+    end
+
+    def products
+    end
+
+    def health
+    end
+
+    def business
+    end
+
+    def blog
+    end
+
+    def contact
+    end
+
+    def aboutus
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'pages#home'
+  get '/products', to: 'pages#products'
+  get '/health' ,to: 'pages#health'
+  get '/business', to: 'pages#business'
+  get '/blog' ,to: 'pages#blog'
+  get '/contact', to: 'pages#contact'
+  get '/aboutus', to: 'pages#aboutus'
+  get '/admin', to: 'admin#login'
+  post '/admin', to: 'admin#login' # Handle login form submission
 end
